3bb9fbbe1555d1ef53d3d32e513a82bce0b06ef5
shouma237 <jonatha.mariano15@gmail.com>
2024-03-22 08:28:50 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const hoursEl = document.getElementById("hour");
+const minuteEl = document.getElementById("minutes");
+const secondEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm");
+
+function updateClock() {
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+
+  if (h > 12) {
+    h = h - 12;
+    ampm = "PM";
+  }
+
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  hoursEl.innerText = h;
+  minuteEl.innerText = m;
+  secondEl.innerText = s;
+  ampmEl, (innerText = ampm);
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
